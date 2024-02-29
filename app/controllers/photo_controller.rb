5cf73fc4ef8a21ca59b4e145d9ac2c858f52d2d6
occiandiaali <ocean.diaali@gmail.com>
2024-02-29 13:34:22 +0100

    add cloudinary and yml file to config

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,11 +1,28 @@
 
+    # class PhotoController < ApplicationController
+    #   def store
+    #     render plain: params[:caption].inspect
+    #   end
+
+    #   def index
+    #     @posts = Post.all
+    #   end
+    # end
+
     class PhotoController < ApplicationController
-      def store
-        render plain: params[:caption].inspect
-      end
 
       def index
-        @posts = Post.all
+        @posts = Post.all.order("created_at DESC")
+      end
+
+      def store
+        # upload image to cloudinary
+        @value = Cloudinary::Uploader.upload(params[:image])
+        # create a new post object and save to db
+        @post = Post.new({:link => @value['secure_url'], :caption => params[:caption]})
+        @post.save
+        # trigger an event with pusher
+       # [...]
+        redirect_to('/')
       end
     end
-
